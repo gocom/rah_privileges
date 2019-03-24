b65037ba874a62fa5e9dea78e1debfa225c3404e
Jukka Svahn <void@rah.pw>
2019-03-24 03:27:11 +0200

    Use txp-grid

--- src/Rah/Functions.php
+++ src/Rah/Functions.php
@@ -40,6 +40,7 @@ function rah_privileges_input($name, $value)
 
     unset($levels[0]);
     $out[] = hInput($field, $resource);
+    $out[] = tag_start('div', ['class' => 'txp-grid']);
 
     foreach ($levels as $group => $label) {
         $id = $name . '_' . intval($group);
@@ -54,12 +55,11 @@ function rah_privileges_input($name, $value)
                 $id
             ) . ' ' .
             tag($label, 'label', array('for' => $id)),
-            'span',
-            [
-                'style' => 'white-space: nowrap',
-            ]
+            'span'
         ) . ' ';
     }
+
+    $out[] = tag_end('div');
 
     return implode('', $out);
 }
